2598d3ce7c023f209714d7cfa64e33d3b99dd6d2
Hirohito Tsuru <hirohito.tsuru@tri-ad.global>
2020-08-06 11:28:18 +0900

    init

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Amplify from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+      identityPoolId: 'ap-northeast-1:86cb43d0-b395-423d-872b-ee4c1ae80c76',
+      region: 'ap-northeast-1',
+      userPoolId: 'ap-northeast-1_pTBR7ClMx',
+      userPoolWebClientId: '1nmiq1alt70evn035kbbab3thu'
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
